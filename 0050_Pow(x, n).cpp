efd5ce4cd3f12a7e1d6f6a1d18e5fa9ffde80a2e
ANITA-0604 <71278293+ANITA-0604@users.noreply.github.com>
2023-05-06 01:59:32 +0800

    Update 0050_Pow(x, n).cpp

--- 0050_Pow(x, n).cpp
+++ 0050_Pow(x, n).cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        cout<<n<<endl;
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        if(n == -1) return 1.0/x;
+        double y = myPow(x,n/2);
+        // cout<<n%2<<endl;
+        if(n % 2 == 1 ) {
+            // cout<<"11111"<<endl;
+            // cout<<y*y*x<<endl;
+            return y*y*x;
+        }
+        if(n % 2 == -1) {
+            // cout<<"222222"<<endl;
+            // cout<<y*y/x<<endl;
+            return y*y/x;
+        }
+        // cout<<y*y<<endl;
+        return y*y;
     }
 };
